81f12c2445a69d887593f1e6bef57e0ae8c53eb9
Riju Dasgupta <63913601+RijuDasgupta9116@users.noreply.github.com>
2023-06-03 21:58:34 +0530

    Create Review.js

---
+++ src/components/Review/Review.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Review.css'
+export default function Review() {
+  return (
+    <>
+
+<div className="user-review container mt-5">
+        <div className="d-flex justify-content-center">
+            <i className="fa fa-star-o mx-3 margin-top-10" aria-hidden="true"></i>
+            <h3 className="margin-top-10">User Review</h3>
+            <i className="fa fa-star-o mx-3 margin-top-10" aria-hidden="true"></i>
+        </div>
+
+        <div className="review-box text-center mt-5">
+            <i className="fa fa-star mt-5 size-40" aria-hidden="true"></i>
+
+            <i className="fa fa-star size-40" aria-hidden="true"></i>
+
+            <i className="fa fa-star size-40" aria-hidden="true"></i>
+
+            <i className="fa fa-star size-40" aria-hidden="true"></i>
+
+            <i className="fa fa-star-half-o size-40" aria-hidden="true"></i>
+
+            <br/>
+            <br/>
+            <p className="justify-center width-80">Explore the Mysteries of the Universe with Our Expert Astrology Service Explore the Mysteries of the Universe with Our Expert Astrology Service Explore the Mysteries of the Universe with Our Expert Astrology Service</p>
+        </div>
+
+    </div>
+    
+    </>
+  )
+}
