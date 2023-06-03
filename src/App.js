51a7361ab826799880965de0b995e974427531fd
Riju Dasgupta <63913601+RijuDasgupta9116@users.noreply.github.com>
2023-06-03 21:54:12 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Review from './components/Review/Review';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Review/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
